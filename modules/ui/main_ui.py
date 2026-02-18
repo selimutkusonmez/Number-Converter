d3933740f51172639ff899e12965cc647bb70c95
Selim Utku Sönmez <selimutkusonmez70@gmail.com>
2026-02-18 13:45:19 +0300

    Menu bar added

--- modules/ui/main_ui.py
+++ modules/ui/main_ui.py
@@ -1,8 +1,9 @@
 import sys
+import subprocess
 from PyQt6.QtCore import Qt,QRegularExpression,QSize
 from PyQt6.QtWidgets import (
      QApplication,QWidget,QMainWindow,QLineEdit,QPushButton,QTextEdit,QLabel,QGridLayout,QFrame,QTableWidget,QTableWidgetItem,QGroupBox,QComboBox,QMessageBox,QFileDialog,QListWidget,QTabWidget,QVBoxLayout,QStatusBar,QSizePolicy,QHBoxLayout)
-from PyQt6.QtGui import QIcon,QPixmap,QIntValidator,QDoubleValidator,QRegularExpressionValidator,QKeyEvent,QPainter,QFontDatabase,QFont
+from PyQt6.QtGui import QIcon,QPixmap,QIntValidator,QDoubleValidator,QRegularExpressionValidator,QKeyEvent,QPainter,QFontDatabase,QFont,QAction,QActionGroup
 
 from modules.assets.style.style_reader import read_style
 
@@ -10,6 +11,7 @@ class MainUI(QMainWindow):
     def __init__(self):
         super().__init__()
         self.init_ui()
+        self.light_theme_action_function()
     
     def init_ui(self):
 
@@ -18,6 +20,7 @@ class MainUI(QMainWindow):
 
         self.main_ui_central_widget = QWidget()
         self.setCentralWidget(self.main_ui_central_widget)
+
         self.main_ui_central_widget_layout = QGridLayout()
         self.main_ui_central_widget.setLayout(self.main_ui_central_widget_layout)
 
@@ -25,10 +28,12 @@ class MainUI(QMainWindow):
         self.label.setObjectName("main_label")
         self.main_ui_central_widget_layout.addWidget(self.label,0,0,Qt.AlignmentFlag.AlignCenter)
 
+        #Upper Groupbox
         self.upper_groupbox = QGroupBox("Input")
         self.upper_groupbox.setFixedHeight(125)
         self.upper_groupbox.setProperty("class","groupbox")
         self.setObjectName("upper_groupbox")
+
         self.upper_groupbox_layout = QHBoxLayout()
         self.upper_groupbox.setLayout(self.upper_groupbox_layout)
         self.main_ui_central_widget_layout.addWidget(self.upper_groupbox,1,0)
@@ -46,14 +51,15 @@ class MainUI(QMainWindow):
         self.convert_button = QPushButton("Convert")
         self.upper_groupbox_layout.addWidget(self.convert_button,1)
 
+        #Lower Groupbox
         self.lower_groupbox = QGroupBox("Output")
         self.lower_groupbox.setProperty("class","groupbox")
         self.setObjectName("lower_groupbox")
         self.lower_groupbox.setFixedHeight(100)
+
         self.lower_groupbox_layout = QGridLayout()
         self.lower_groupbox.setLayout(self.lower_groupbox_layout)
         self.main_ui_central_widget_layout.addWidget(self.lower_groupbox,2,0)
-        self.lower_groupbox_layout.setVerticalSpacing(1)
 
         self.output_1_label = QLabel("Decimal")
         self.output_1_label.setProperty("class","label")
@@ -85,6 +91,44 @@ class MainUI(QMainWindow):
         self.setStatusBar(QStatusBar())
         self.statusBar().showMessage("App Ready")
 
-        self.setStyleSheet(read_style("main_ui.qss"))
+        #MenuBar
+        menu_bar = self.menuBar()
+
+        #File Menu
+        file_menu = menu_bar.addMenu("File")
+        restart_app_action = QAction("Restart App",self)
+        restart_app_action.setShortcut("Ctrl+R")
+        restart_app_action.triggered.connect(self.restart_app_action_function)
+        file_menu.addAction(restart_app_action)
+
+        #Settings Menu
+        settings_menu = menu_bar.addMenu("Settings")
+        theme_menu = settings_menu.addMenu("Theme")
+        theme_action_group = QActionGroup(self)
+
+        light_theme_action = QAction("Light Theme")
+        light_theme_action.setShortcut("Ctrl+L")
+        light_theme_action.setChecked(True)
+        light_theme_action.setCheckable(True)
+        theme_menu.addAction(light_theme_action)
+        theme_action_group.addAction(light_theme_action)
+        light_theme_action.triggered.connect(self.light_theme_action_function)
+
+        dark_theme_action = QAction("Dark Theme")
+        dark_theme_action.setShortcut("Ctrl+D")
+        dark_theme_action.setCheckable(True)
+        theme_menu.addAction(dark_theme_action)
+        theme_action_group.addAction(dark_theme_action)
+        dark_theme_action.triggered.connect(self.dark_theme_action_function)
+
+    def restart_app_action_function(self):
+        QApplication.quit()
+        subprocess.Popen([sys.executable, *sys.argv])
+    
+    def dark_theme_action_function(self):
+        self.setStyleSheet(read_style("main_ui_dark_theme.qss"))
+
+    def light_theme_action_function(self):
+        self.setStyleSheet(read_style("main_ui_light_theme.qss"))
 
         
